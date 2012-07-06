847c660528584188d841f3170956ab0d64fc4559
eguvenc <xturknet@hotmail.com>
2012-07-06 19:01:15 +0300

    fixed loaing layout bug in view library

---
+++ modules/views/layouts/welcome.php
@@ -0,0 +1,23 @@
+<?php echo doctype('xhtml11')."\n"; ?>
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" >
+    
+<head>
+<?php
+echo meta('Content-type', 'text/html; charset=utf-8', 'equiv');
+echo meta('description', '');
+echo meta('author', '');
+?>
+<?php echo view_var('meta'); ?>
+
+<title><?php echo strip_tags(view_var('title')); ?></title>
+
+<?php echo view_var('head'); ?>
+
+</head>
+
+<body>
+
+<?php echo view_var('body'); ?>
+
+</body>
+</html>
